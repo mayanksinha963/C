02c9379fba7900f331c58269cec2477d230766fa
Mayank Sinha <114310294+mayanksinha963@users.noreply.github.com>
2024-02-01 20:14:26 +0530

    gcd

---
+++ Practical Lab/50 questions of C/gcd.c
@@ -0,0 +1,24 @@
+//Write a  C program to find the GCD of two numbers.
+#include<stdio.h>
+
+int gcd (int num1, int num2)
+{
+    if (num2 == 0)
+    {
+        return num1;
+    }
+    else
+    {
+        return gcd(num2, num1%num2);
+    }
+    
+}
+int main(int argc, char const *argv[])
+{
+    int num1 = 56, num2 = 98;
+    int result = gcd(num1, num2);
+    printf("%d", result);
+    return 0;
+
+    abs()
+}
