b39c05ffb1e43e96390a8cc5b35f38b28e60d05e
Mayank Sinha <114310294+mayanksinha963@users.noreply.github.com>
2024-02-01 22:00:20 +0530

    code without semicolon

--- Practical Lab/50 questions of C/without_semicolon.c
+++ Practical Lab/50 questions of C/without_semicolon.c
@@ -1,4 +1,4 @@
-//Write a Program in C to Print all natural numbers up to N without using a semi-colon.
+//Write a Programing code in C to Print all natural numbers up to N without using a semi-colon.
 #include<stdio.h>
 #define N 10
 int main(int val)
