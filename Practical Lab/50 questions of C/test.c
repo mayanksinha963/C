7b7fafd4df321777b76679355556ef7125d909ea
Mayank Sinha <114310294+mayanksinha963@users.noreply.github.com>
2024-02-09 16:59:21 +0530

    question

--- Practical Lab/50 questions of C/test.c
+++ Practical Lab/50 questions of C/test.c
@@ -20,8 +20,33 @@ int main(int argc, char const *argv[])
     // int a = 30*1000 + 2768;
     // printf("%d",a);
     \
-    int INT = 32769;
-    float FLOAT = 1.35e153;
-    printf("INT = %d FLOAT = %f\n", INT, FLOAT);
+    // int INT = 32769;
+    // float FLOAT = 1.35e153;
+    // printf("INT = %d FLOAT = %f\n", INT, FLOAT);
+
+    // printf("%d %c\n");
+    // printf("%d %c\n");
+
+    // printf("%f %f");
+
+    // int x,y;
+    // x = -15--20;
+    // y = -15--(-20);
+    // printf("x = %d y = %d", x,y); 
+
+    // float x =13, y =22;
+    // float a;
+    // a = x%y;
+    // printf("%d",a);
+
+    // int a = 200*200/200;
+    // printf("%d",a );
+
+    float x = 1.5;
+    int y = 3;
+    // x = y/2+y*8/y-y+x/3;
+    // printf("x=%f\n",x);
+    float z = 3.00/2;
+    printf("%f", z);
     return 0;
 }
