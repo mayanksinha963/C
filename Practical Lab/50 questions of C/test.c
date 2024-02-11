5040b124f9f610fa2d59eb29c2309d62b82d0d83
Mayank Sinha <114310294+mayanksinha963@users.noreply.github.com>
2024-02-12 01:29:03 +0530

    test

--- Practical Lab/50 questions of C/test.c
+++ Practical Lab/50 questions of C/test.c
@@ -1,86 +1,55 @@
 #include<stdio.h>
+#include<string.h>
+// union 
+// {
+//     double a;
+//     int b[10];
+//     char c;
+// }p;
+
+// union  p
+//     {
+//         int x; 
+//         int y;
+//     }k ={.y = 97};
+
+    // typedef struct p* q*;
+
+    // #if X == 3;
 
 int main(int argc, char const *argv[])
 {
-    // int a = 5, b =7;
-    // float c,d;
-    // c = a/b*b;
-    // d = b/a*a;
-    // printf("%f %f", c,d);
+    // p.y = 60;
+    // printf("%d", sizeof(p));
+    // printf("%d",sizeof(p));
+    // union p p,b;
 
-    // int a = 'A';
-    // printf("%d",a);
-
-    // char b = 'B' + 17;
-    // printf("%c", b);
-    // float f;
-    // scanf("Enter No. %f", &f);
-    // printf("%f", f);
-
-    // int a = 30*1000 + 2768;
-    // printf("%d",a);
-    \
-    // int INT = 32769;
-    // float FLOAT = 1.35e153;
-    // printf("INT = %d FLOAT = %f\n", INT, FLOAT);
-
-    // printf("%d %c\n");
-    // printf("%d %c\n");
-
-    // printf("%f %f");
-
-    // int x,y;
-    // x = -15--20;
-    // y = -15--(-20);
-    // printf("x = %d y = %d", x,y); 
-
-    // float x =13, y =22;
-    // float a;
-    // a = x%y;
-    // printf("%d",a);
-
-    // int a = 200*200/200;
-    // printf("%d",a );
-
-    // float x = 1.5;
-    // int y = 3;
-    // // x = y/2+y*8/y-y+x/3;
-    // // printf("x=%f\n",x);
-    // float z = 3.00/2;
-    // printf("%f", z);
-
-    // float x;
-    // x = 6/3;
-    // printf("%d %f",7,7.0);
-    int p, q;
-    // scanf("Enter values of p and q %d%d", &p, &q);
-
-    // printf("You Entered p : %d q = %d", p, q);
-    // scanf(" %d %d", &p, &q);
-    // printf("%d %d", p, q);
-    // scanf("%d\n\n%d", &p,&q);
-
-    // printf("none can nt really do anything if somone is\
-    // passing through a difficult period. But, often small words\
-    // of support help people snap out of their ");
-
-    // printf("%d %d", p, q);
-
-    // printf("This is a long message that "
-    //    "spans over multiple lines "
-    //    "for better readability.\n");
-
-    // int i = 7, j = -2, k= 0, x,y;
-    // x = i-7 && j+5 || k+2;
-    // y = i+5 || j+1 && k+2;
-    // printf("%d %d", x, y);
-
-    int i;
-    for (int i = 1; i++ <= 5; printf("%d",i))
-    {
-        printf("\nHello");
-    }
+    // for(; ;);
+    // for(; ;)
+    // {
+    //     printf("Hello");
+    // }
     
+    // printf("%d",k.y);
+    // struct p p1[] = {1,2,3,4,5,6};
+    // printf("%d",sizeof(p1));
+    // struct student s;
+    // s.no = 8;
+    // // printf("%d", s.no);
+
+    // int i =0;
+    // for (int i = 0; i < 5; ++i++)
+    // {
+    //     printf("Hello");
+    // }
+
+
+    const char str1[] ="abcdef";
+    const char str2[] = "fgha";
+
+    char * mat;
+    mat = strpbrk(str1, str2);
+    printf("%c",*mat);
 
     return 0;
 }
