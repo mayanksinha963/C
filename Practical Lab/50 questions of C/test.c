9bd30041f4ab1eeb637d857c0e2909de1124d95a
Mayank Sinha <114310294+mayanksinha963@users.noreply.github.com>
2024-02-13 04:18:51 +0530

    test

--- Practical Lab/50 questions of C/test.c
+++ Practical Lab/50 questions of C/test.c
@@ -1,55 +1,16 @@
 #include<stdio.h>
-#include<string.h>
-// union 
+
+#define CUBE(X) X*X*X
+// int cube (int X)
 // {
-//     double a;
-//     int b[10];
-//     char c;
-// }p;
+//     return X*X*X;
+// }
 
-// union  p
-//     {
-//         int x; 
-//         int y;
-//     }k ={.y = 97};
-
-    // typedef struct p* q*;
-
-    // #if X == 3;
 
 int main(int argc, char const *argv[])
 {
-    // p.y = 60;
-    // printf("%d", sizeof(p));
-    // printf("%d",sizeof(p));
-    // union p p,b;
-
-    // for(; ;);
-    // for(; ;)
-    // {
-    //     printf("Hello");
-    // }
-    
-    // printf("%d",k.y);
-    // struct p p1[] = {1,2,3,4,5,6};
-    // printf("%d",sizeof(p1));
-    // struct student s;
-    // s.no = 8;
-    // // printf("%d", s.no);
-
-    // int i =0;
-    // for (int i = 0; i < 5; ++i++)
-    // {
-    //     printf("Hello");
-    // }
-
-
-    const char str1[] ="abcdef";
-    const char str2[] = "fgha";
-
-    char * mat;
-    mat = strpbrk(str1, str2);
-    printf("%c",*mat);
-
+    int a;
+     a= 64/CUBE(4);
+     printf("%d",a);
     return 0;
 }
